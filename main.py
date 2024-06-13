d2c00224f04c08a0cebcd93562707ca71078df5c
DimarashkA <159241659+DimarashkA@users.noreply.github.com>
2024-06-13 16:02:41 +0300

    add icons

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -7,9 +8,18 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load("")
 
+icon = pygame.image.load("img/тир.jpg")
+pygame.display.set_icon(icon)
 
+target_img = pygame.image.load("img/target.png")
+target_widht = 80
+target_height = 80
+
+target_x = random.randint(0,SCREEN_HEIGHT - target_height)
+target_y = random.randint(0,SCREEN_WIDHT - target_widht)
+
+color = random.randint(0, 250), random.randint(0, 250), random.randint(0, 250)
 
 running = True
 
